4ec0633ff347fdd99b9d246be60cc4744a5b4e48
Annisa Lathifa <102348322+lthfaannisa@users.noreply.github.com>
2022-11-27 21:03:27 +0700

    Update javadoc blueberry.java

--- src/main/java/Class/blueberry.java
+++ src/main/java/Class/blueberry.java
@@ -5,44 +5,83 @@
 package Class;
 
 /**
- *
- * @author USER DK
+ * Class blueberry (subclassdari class menu)
+ * memiliki atribut blueberry dan saubTotal
+ * 
  */
 public class blueberry extends menu {
     private int blueberry = 0;
     private int subTotal = 0;
     
+    /**
+    * Memanggil constractor tanpa parameter
+    */
     public blueberry(){
         super();
     }
     
+    /**
+    * memanggil constractor dgn parameter
+    * @param nama
+    * @param harga
+    */
     public blueberry(String nama, int harga){
         super(nama,harga);
     }
     
+    /**
+    * method getter untuk mengakses nilai variable blueberry
+    * @return
+    */
     public int getBlueberry(){
        return this.blueberry;
     }
    
+    /**
+    * method setter untuk mengubah nilai variable blueberry
+    * @param jumlah
+    */
     public void setBlueberry(int jumlah){
        this.blueberry = jumlah;
     }
     
+    /**
+    * menambah jumlah es krim rasa blueberry
+    */
     public void addBlueberry(){
         this.blueberry += 1;
     }
     
+    /**
+    * mengurangi jumlah es krim blueberry
+    */
     public void minBlueberry(){
         this.blueberry -= 1;
     }
     
+    /**
+    * mengembalikan nilai sub total
+    * @return
+    */
     public int getSubTotal(){
        return this.subTotal;
    }
+    
+    /**
+    * mengubah nilai sub Total
+    * @param totalHarga
+    */
    public void setSubTotal(int totalHarga){
        this.subTotal = totalHarga;
    }
     
+    /**
+    * mengembalikan harga sesuai dengan jumlah pesanan
+    * @param jumlah
+    * @param harga
+    * @return
+    * Override untuk menjalankan method abstrak
+    */
     @Override
     public int getTotal(int jumlah, int harga){
         int temp = harga * jumlah;
