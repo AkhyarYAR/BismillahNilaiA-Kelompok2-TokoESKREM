f6c03cb2c061bc817137ff5f567cc95ed19f522b
rahmatul idami <105691553+rahmatulidami@users.noreply.github.com>
2022-11-27 23:33:35 +0700

    Update javadoc Chocolate

--- src/main/java/Class/chocolate.java
+++ src/main/java/Class/chocolate.java
@@ -5,44 +5,83 @@
 package Class;
 
 /**
- *
- * @author USER DK
+ * Class chocolate (subclass dari class menu)
+ * memiliki atribut chocolate dan subTotal
+ * 
  */
 public class chocolate extends menu {
     private int chocolate = 0;
     private int subTotal = 0;
     
+    /**
+    * Memanggil constractor tanpa parameter
+    */
     public chocolate(){
         super();
     }
     
+    **
+    * memanggil constractor dgn parameter
+    * @param nama
+    * @param harga
+    */
     public chocolate(String nama, int harga){
         super(nama,harga);
     }
     
+    /**
+    * method getter untuk mengakses nilai variable Chocolate
+    * @return
+    */
     public int getChocolate(){
        return this.chocolate;
     }
    
+   /**
+    * method setter untuk mengubah nilai variable Chocolate
+    * @param jumlah
+    */
     public void setChocolate(int jumlah){
        this.chocolate = jumlah;
     }
     
+    /**
+    * menambah jumlah Chocolate
+    */
     public void addChocolate(){
         this.chocolate += 1;
     }
     
+    /**
+    * mengurangi jumlah Chocolate
+    */
     public void minChocolate(){
         this.chocolate -= 1;
     }
     
+    /**
+    * mengembalikan nilai sub total
+    * @return
+    */
     public int getSubTotal(){
        return this.subTotal;
    }
+   
+   /**
+    * mengubah nilai sub Total
+    * @param totalHarga
+    */
    public void setSubTotal(int totalHarga){
        this.subTotal = totalHarga;
    }
     
+    /**
+    * mengembalikan harga sesuai dengan jumlah pesanan
+    * @param jumlah
+    * @param harga
+    * @return
+    * Override untuk menjalankan method abstrak
+    */
     @Override
     public int getTotal(int jumlah, int harga){
         int temp = harga * jumlah;
