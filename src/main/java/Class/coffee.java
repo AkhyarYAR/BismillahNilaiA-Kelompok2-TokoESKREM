ece6d2cc653485421f7c9a293ae02a0c7a3706d2
rahmatul idami <105691553+rahmatulidami@users.noreply.github.com>
2022-11-27 23:38:47 +0700

    Update javadoc coffe

--- src/main/java/Class/coffee.java
+++ src/main/java/Class/coffee.java
@@ -5,43 +5,83 @@
 package Class;
 
 /**
- *
- * @author USER DK
+ * Class coffee (subclass dari class menu)
+ * memiliki atribut coffee dan subTotal
+ * 
  */
 public class coffee extends menu {
     private int coffee = 0;
     private int subTotal = 0;
     
+    /**
+    * Memanggil constractor tanpa parameter
+    */
     public coffee(){
         super();
     }
+    
+    /**
+    * memanggil constractor dgn parameter
+    * @param nama
+    * @param harga
+    */
     public coffee(String nama, int harga){
         super(nama,harga);
     }
     
+    /**
+    * method getter untuk mengakses nilai variable Coffee
+    * @return
+    */
     public int getCoffee(){
        return this.coffee;
     }
    
+    /**
+    * method setter untuk mengubah nilai variable Coffee
+    * @param jumlah
+    */
     public void setCoffee(int jumlah){
        this.coffee = jumlah;
     }
     
+    /**
+    * menambah jumlah Coffee
+    */
     public void addCoffee(){
         this.coffee += 1;
     }
     
+    /**
+    * mengurangi jumlah Coffee
+    */
     public void minCoffee(){
         this.coffee -= 1;
     }
     
+    /**
+    * mengembalikan nilai sub total
+    * @return
+    */
     public int getSubTotal(){
        return this.subTotal;
    }
+   
+   /**
+   * mengubah nilai sub Total
+   * @param totalHarga
+   */
    public void setSubTotal(int totalHarga){
        this.subTotal = totalHarga;
    }
     
+    /**
+    * mengembalikan harga sesuai dengan jumlah pesanan
+    * @param jumlah
+    * @param harga
+    * @return
+    * Override untuk menjalankan method abstrak
+    */
     @Override
     public int getTotal(int jumlah, int harga){
         int temp = harga * jumlah;
